0cd3f9e10ffff1382d307d15bee52aed26e9d5f7
everis-sembu <paulakeen@gmail.com>
2019-04-01 01:09:24 +0200

    transformer class working

---
+++ RUN-ME-NOW-classpath.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#### These variables must be exported in the environment for the jupyter notebook to find the *.jar(s) required for the proper execution of the UBLyter notebook.
+
+
+export IJAVA_CLASSPATH=./lib/*.jar
+
+
